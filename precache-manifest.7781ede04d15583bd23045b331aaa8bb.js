b5aac79ac26d8186e956f3bf663543740b426eb1
Tariq <tguesri@gmail.com>
2018-11-30 17:11:00 +0100

    Updates

---
+++ precache-manifest.7781ede04d15583bd23045b331aaa8bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "e9948aa42fdc0fe0ce4a",
+    "url": "/contactmanager/static/js/main.e9948aa4.chunk.js"
+  },
+  {
+    "revision": "e7fbf390f1866b0facde",
+    "url": "/contactmanager/static/js/1.e7fbf390.chunk.js"
+  },
+  {
+    "revision": "e9948aa42fdc0fe0ce4a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e7fbf390f1866b0facde",
+    "url": "/contactmanager/static/css/1.d0c06513.chunk.css"
+  },
+  {
+    "revision": "35d03fc14746e8f01ef7203c5b8abcd1",
+    "url": "/contactmanager/index.html"
+  }
+];
